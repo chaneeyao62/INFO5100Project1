3070846c47652f732aacb93d8592b423c33f1fc1
Qianyan Yao <yaoqianyan@gmail.com>
2017-03-07 10:18:29 -0500

    third plot/histogram

--- tech_price_index.R
+++ tech_price_index.R
@@ -9,3 +9,6 @@ MacBook_wo_V = tech[,5][-length(tech[,5])]
 hist(MacBook_wo_V, xlab = "Price", main = "Price of MacBook w/o Venezuela")
 Windows.Powered_wo_V = tech[,6][-length(tech[,6])]
 hist(Windows.Powered_wo_V, xlab = "Price", main = "Price of Windows.Powered w/o Venezuela")
+
+Xbox_wo_V = tech[,8][-1]
+hist(Xbox_wo_V, xlab = "Price",breaks = 6,xlim = c(0,3000))
